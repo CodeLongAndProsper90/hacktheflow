9056b797e81412b580a840435945c34f2a837d7d
Ellie Little <jupiter@m.rdis.dev>
2023-03-10 18:10:38 -0600

    setup dev env

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "My Android project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    android-nixpkgs.url = "github:tadfisher/android-nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, android-nixpkgs }: 
+  let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.android_sdk.accept_license = true;
+      config.allowUnfree = true;
+    };
+    android = android-nixpkgs.sdk (sp: with sp; [
+          cmdline-tools-latest
+          build-tools-32-0-0
+          flatform-tools
+          platforms-android-31
+          emulator
+        ]);
+  in {
+    devShell.x86_64-linux = (pkgs.buildFHSUserEnv {
+      name = "android-sdk-env";
+      targetPkgs = pkgs: (with pkgs; [
+        gradle
+        #androidenv.androidPkgs_9_0.androidsdk
+        flutter
+        glibc
+        fish
+        dart
+        kotlin
+        jdk11
+      ]);
+      profile = ''
+        export JAVA_HOME=${pkgs.jdk11.home}
+      '';
+      runScript = "bash -c fish";
+    }).env;
+  };
+}
