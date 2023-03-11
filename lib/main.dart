f04b95a52a611e826724e64e9671c054e6cd8223
Joey Lent <supercolbat@gmail.com>
2023-03-11 06:40:44 -0600

    Fix broken import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hacktheflow/colors.dart';
 import 'package:hacktheflow/pages/onboarding.dart';
-import 'package:hacktheflow/pages/signup.dart';
 import 'package:hacktheflow/pages/home.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
