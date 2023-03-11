1634c2205943dbb82f174a99b39ae7fad077fd70
Scott Little <jupiter@m.rdis.dev>
2023-03-11 11:56:17 -0600

    make chat work

--- lib/backend/user.dart
+++ lib/backend/user.dart
@@ -3,35 +3,42 @@ import 'dart:typed_data';
 import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-final _supabase = Supabase.instance.client;
+final supabase = Supabase.instance.client;
 
-class User {
+class AppUser {
   String id;
   String username;
   int zip;
+	String name;
 
-  User({required this.id, required this.username, required this.zip});
+  AppUser({required this.id, required this.username, required this.zip, required this.name});
 
-  User.fromJSON(Map<String, dynamic> d)
+  AppUser.fromJSON(Map<String, dynamic> d)
       : id = d["id"],
         username = d["username"],
+				name = d["name"],
         zip = d["zip"];
 
   Map<String, dynamic> toJSON() {
-    return {"id": id, "username": username, "zip": zip};
+    return {"id": id, "username": username, "zip": zip, "name": name};
   }
 
   Future<Uint8List> getAvatar() async {
-    return await _supabase.storage.from("avatar").download("$id/avatar.png");
+    return await supabase.storage.from("avatar").download("$id/avatar.png");
   }
 
   Future<void> setAvatar(XFile img) async {
     final data = await img.readAsBytes();
     String path = "$id/avatar.png";
-    await _supabase.storage.from("avatar").uploadBinary(
+    await supabase.storage.from("avatar").uploadBinary(
           path,
           data,
           fileOptions: FileOptions(contentType: img.mimeType),
         );
   }
+}
+
+Future<AppUser> getUser(String id) async {
+	var data = await supabase.from("profiles").select<List<Map<String, dynamic>>>().eq("id", id);
+	return AppUser.fromJSON(data[0]);
 }
