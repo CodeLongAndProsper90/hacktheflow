2092dddd7505303d51d81d15ecf76cb295cf224b
Joey Lent <supercolbat@gmail.com>
2023-03-11 09:19:36 -0600

    Average navbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hacktheflow/widgets/navbar.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:hacktheflow/backend/user.dart';
 import 'package:hacktheflow/main.dart';
@@ -24,30 +25,40 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Text("Main page"),
-            TextField(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text("Main page"),
+              TextField(
                 decoration: InputDecoration(labelText: "message"),
-                controller: msgCon),
-            TextButton(
+                controller: msgCon,
+              ),
+              TextButton(
                 child: Text("Send"),
                 onPressed: () async {
                   await send(
-                      to: "b7891604-2cff-48bb-ad00-bc9097af1086",
-                      contents: msgCon.text);
-                }),
-            TextButton(
+                    to: "b7891604-2cff-48bb-ad00-bc9097af1086",
+                    contents: msgCon.text,
+                  );
+                },
+              ),
+              TextButton(
                 child: Text("Sign out"),
                 onPressed: () async {
                   await supabase.auth.signOut();
                   Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => StartPage()));
-                })
-          ],
+                    MaterialPageRoute(
+                      builder: (context) => StartPage(),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
+      bottomNavigationBar: const Navbar(),
     );
   }
 }
