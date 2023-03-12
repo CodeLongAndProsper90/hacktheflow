c24106e5a0334892cce4824895d3bf184f618764
Joey Lent <supercolbat@gmail.com>
2023-03-11 20:09:54 -0600

    Stamping out a small fire in a burning house

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -40,65 +40,66 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Navbar(
-      onChange: (index) {
-        setState(() {
-          _pageIndex = index;
-        });
-      },
-      child: SafeArea(
-        child: [
-          // Mainpage(),
-          // Messanger(personconnected: 'Jane Doe'),
-          HomeDiscoverPage(),
-          HomeOpenChatsPage(),
-          // MessagingHub(),
-          // HomeMessagesPage(to_id: "b7891604-2cff-48bb-ad00-bc9097af1086"),
-          HomeProfilePage(),
-        ][_pageIndex],
-      ),
-    ));
-  }
-}
-
-class Mainpage extends StatefulWidget {
-  const Mainpage({Key? key}) : super(key: key);
-
-  @override
-  State<Mainpage> createState() => _MainpageState();
-}
-
-class _MainpageState extends State<Mainpage> {
-  final ImagePicker _p = ImagePicker();
-  List<XFile> images = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Text("Main page"),
-          TextButton(
-              child: Text("Take photo"),
-              onPressed: () async {
-                XFile? img = await _p.pickImage(source: ImageSource.camera);
-                if (img == null) return;
-                XFile image = img!;
-                images.add(image);
-              }),
-          TextButton(
-            child: Text("Sign out"),
-            onPressed: () async {
-              await supabase.auth.signOut();
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => StartPage(),
-                ),
-              );
-            },
-          ),
-        ],
+      body: Navbar(
+        onChange: (index) {
+          setState(() {
+            _pageIndex = index;
+          });
+        },
+        child: SafeArea(
+          child: [
+            // Mainpage(),
+            // Messanger(personconnected: 'Jane Doe'),
+            const HomeDiscoverPage(),
+            const HomeOpenChatsPage(),
+            // MessagingHub(),
+            // HomeMessagesPage(to_id: "b7891604-2cff-48bb-ad00-bc9097af1086"),
+            const HomeProfilePage(),
+          ][_pageIndex],
+        ),
       ),
     );
   }
 }
+
+// class Mainpage extends StatefulWidget {
+//   const Mainpage({Key? key}) : super(key: key);
+
+//   @override
+//   State<Mainpage> createState() => _MainpageState();
+// }
+
+// class _MainpageState extends State<Mainpage> {
+//   final ImagePicker _p = ImagePicker();
+//   List<XFile> images = [];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Column(
+//         children: [
+//           Text("Main page"),
+//           TextButton(
+//               child: Text("Take photo"),
+//               onPressed: () async {
+//                 XFile? img = await _p.pickImage(source: ImageSource.camera);
+//                 if (img == null) return;
+//                 XFile image = img!;
+//                 images.add(image);
+//               }),
+//           TextButton(
+//             child: Text("Sign out"),
+//             onPressed: () async {
+//               await supabase.auth.signOut();
+//               Navigator.of(context).push(
+//                 MaterialPageRoute(
+//                   builder: (context) => StartPage(),
+//                 ),
+//               );
+//             },
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
