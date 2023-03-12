d52e9389dcafdae2b9df9b160318bd1fba9924a8
ANGELOREYES-afk <108246492+ANGELOREYES-afk@users.noreply.github.com>
2023-03-11 18:12:03 -0800

    messageing hub + animated the preview

--- lib/pages/messaginghub.dart
+++ lib/pages/messaginghub.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hacktheflow/widgets/styled_text.dart';
 import '../colors.dart';
+import '../widgets/messagepreview.dart';
 import '../widgets/messaging_room.dart';
 
-class MessagingHub extends StatelessWidget {
+class MessagingHub extends StatefulWidget {
   // final List<Map<int, dynamic>> list_of_people;
   final namesList = ["charles", "mary", "john"];
   final Map<String, String> listOfPeople = {
@@ -11,10 +12,11 @@ class MessagingHub extends StatelessWidget {
     "mary": "descriptions2",
     "john": "descriptions3"
   };
-
+  @override
+  MessagingHub createState() => MessagingHub();}
+  class MessagingHub extends State<MessagingHub> {
   // list -> int ->  to list of name of person and his description
-  MessagingHub({super.key});
-
+  bool _isVisible = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +33,7 @@ class MessagingHub extends StatelessWidget {
         ],
       ),
       body: ListView.builder(
-        itemCount: listOfPeople.length,
+        itemCount: 2,
         itemBuilder: (BuildContext context, int index) {
           return SizedBox(
             height: MediaQuery.of(context).size.height * 0.33,
@@ -47,29 +49,37 @@ class MessagingHub extends StatelessWidget {
                   ),
                   child: Row(
                     children: [
-                      GestureDetector(
-                        onTap: () {
-                          Messanger(
-                            personconnected: listOfPeople[index],
-                          );
-                        },
-                        child: const Icon(Icons.message_outlined),
-                      ),
+                      
                       const Divider(
                         thickness: 2.0,
                         color: colorForegroundAlt,
                       ),
                       Column(
+                        // ignore: prefer_const_literals_to_create_immutables
                         children: [
-                          HeaderText(text: namesList[index]),
+                          const HeaderText(text: "George"),
                           const SizedBox(height: 5),
-                          SmallText(listOfPeople[namesList[index]] as String)
+                          const SmallText("listOfPeople[namesList[index]] as String")
                         ],
                       ),
                     ],
                   ),
                 ),
                 const Spacer(),
+                ElevatedButton(
+                        onPressed: () {
+                          // ignore: invalid_use_of_protected_member
+                          setState(() {
+                            _isVisible = !_isVisible;
+                          });
+                        },
+                        child: const Icon(Icons.message_outlined),
+                      ),
+                AnimatedOpacity(
+                  opacity: _isVisible ? 1.0 : 0.0, 
+                  duration: const Duration(milliseconds: 200), 
+                  child: Messagepreveiw(),
+                  )
               ],
             ),
           );
@@ -77,4 +87,5 @@ class MessagingHub extends StatelessWidget {
       ),
     );
   }
+}
 }
